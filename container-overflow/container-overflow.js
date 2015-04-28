f7503b6a7b9eab3cac299817d0f0b1f3ea2ba2df
Eric Constantinides <eric@flickerbox.com>
2015-04-27 19:25:34 -0700

    Updates container-overflow for left and right side overflowing

--- container-overflow/container-overflow.js
+++ container-overflow/container-overflow.js
@@ -12,54 +12,96 @@ new ContainerOverflow();
 function ContainerOverflow() {
 	// Variables
 	var $containerOverflow = $('.js-container-overflow');
+	var $containerOverflowContainer;
+	var $containerOverflowItems;
 	var $containerOverflowItem;
-	var containerOverflowDirection;
-	var containerOverflowLeftPosition;
-	var containerOverflowRightPosition;
-	var containerOverflowWidth = $('.js-container-overflow').width();
-	var thisPos;
-	var $this;
-	var $containerOverflowSide;
-	var $window            = $(window);
-	var windowWidth        = $window.width();
-	var init               = _init();
+	var containerOverflowWidths = [];
+	var containerOverflowItemLeftPosition;
+	var containerOverflowItemRightPosition;
+	var containerOverflowRight = false;
+	var containerOverflowLeft = false;
+	var containerOverflowContainerWidth;
+	var containerOverflowContainerLeft;
+	var containerOverflowContainerRight;
 
-	var windowRightPostion = $window.width();
+	var $window            = $(window);
+	var windowWidth;
+	var init               = _init();
 
 	function _init() {
 		// main();
 		$window.load( main );
 		$window.resize( function() {
-			main(false);
+			main();
 		});
 	}
 
 	function main() {
 		// reset and hide:
-		$containerOverflow.addClass('js-container-overflow__loading').attr('style', '');
 
 		$containerOverflow.each(function(index,el) {
-			$containerOverflowItem = $(this);
-			containerOverflowLeftPosition = $containerOverflowItem.offset().left;
-			containerOverflowRightPosition = $containerOverflowItem.offset().left + $containerOverflowItem.width();
+			$containerOverflowContainer = $(this);
 
-			if ($containerOverflowItem.hasClass('js-container-overflow__go-left')) {
-				$containerOverflowItem.css({
-					left: (containerOverflowLeftPosition * -1) +'px',
-					width: containerOverflowRightPosition
+			$containerOverflowItems = $containerOverflowContainer.find('[class*=js-container-overflow__item]');
+
+
+			//reset:
+			containerOverflowRight = containerOverflowLeft = false;
+			$containerOverflowContainer.removeClass('js-container-overflow__is-loaded').addClass('js-container-overflow__is-loading').attr('style', '');
+			for (var i = 0; i < $containerOverflowItems.length; i++) {
+				$($containerOverflowItems[i]).addClass('js-container-overflow__is-loading').attr('style', '');
+			};
+			containerOverflowWidths = [];
+
+			windowWidth        = Math.ceil($window.width());
+			containerOverflowContainerWidth = Math.ceil($containerOverflowContainer.width());
+			containerOverflowContainerLeft =  Math.floor($containerOverflowContainer.offset().left);
+			containerOverflowContainerRight = containerOverflowContainerLeft + containerOverflowContainerWidth;
+
+			$containerOverflowItems.each(function(index, el) {
+				$containerOverflowItem = $(this);
+
+				containerOverflowItemLeftPosition = Math.floor($containerOverflowItem.offset().left);
+				containerOverflowItemRightPosition = containerOverflowItemLeftPosition + Math.floor($containerOverflowItem.width());
+
+
+				if ($containerOverflowItem.hasClass('js-container-overflow__item--go-left')) {
+					containerOverflowLeft = true;
+					containerOverflowWidths.push(Math.floor( containerOverflowItemRightPosition ));
+				} else if ($containerOverflowItem.hasClass('js-container-overflow__item--go-right')) {
+					containerOverflowRight = true;
+					containerOverflowWidths.push(Math.floor( windowWidth - containerOverflowItemLeftPosition ));
+				} else {
+					containerOverflowWidths.push(Math.floor( $containerOverflowItem.width() ));
+				}
+			});
+
+			// set the widths
+			for (var i = 0; i < $containerOverflowItems.length; i++) {
+				$($containerOverflowItems[i]).css('width', containerOverflowWidths[i] + 'px');
+			};
+
+			if (containerOverflowLeft === true) {
+				$containerOverflowContainer.css({
+					marginLeft: '0',
+					width: (containerOverflowContainerRight + 1)
 				});
-			} else {
-				console.log('containerOverflowRightPosition: ' + containerOverflowRightPosition);
-				console.log('windowRightPostion:             ' + windowRightPostion);
-				$containerOverflowItem.css({
-					right: (containerOverflowRightPosition - windowRightPostion) + 'px',
-					width: (windowRightPostion - containerOverflowRightPosition + $containerOverflowItem.width()) + 'px'
-				});
-				containerOverflowDirection = 'right';
 			}
+			if (containerOverflowRight === true) {
+				$containerOverflowContainer.css({
+					marginRight: '0',
+					width: (windowWidth - containerOverflowContainerLeft + 1)
+				});
+			}
+			if (containerOverflowLeft === true && containerOverflowRight === true) {
+				$containerOverflowContainer.css('width', windowWidth);
+			}
+
+			// restore:
+			$containerOverflowItems.removeClass('js-container-overflow__is-loading');
+			$containerOverflowContainer.removeClass('js-container-overflow__is-loading').addClass('js-container-overflow__is-loaded');
+
 		});
 
-		// restore:
-		$containerOverflow.removeClass('js-container-overflow__loading');
 	}
 }
